b173f16ed7721af12017c9b42a568c68233e453c
rafal1018 <rafaljaneczek@o2.pl>
2021-04-30 23:22:49 +0200

    Initial commit

---
+++ src/main/java/CRUDRestApi2/CrudRestApi2Application.java
@@ -0,0 +1,13 @@
+package CRUDRestApi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRestApi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudRestApi2Application.class, args);
+	}
+
+}
---
+++ src/test/java/CRUDRestApi2/CrudRestApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package CRUDRestApi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRestApi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
